b20a798c4fbd7df498c25f19f6938789c823f85d
Anton Matosov <anton.matosov@gmail.com>
2017-07-11 00:10:07 -0700

    Use custom conan repo

--- .travis/install.sh
+++ .travis/install.sh
@@ -22,10 +22,24 @@ if [[ "$(uname -s)" == 'Darwin' ]]; then
     pyenv activate conan
 fi
 
-pip install conan --upgrade
-pip install conan_package_tools
+# pip install conan --upgrade
+# pip install conan_package_tools
+
+git clone git@github.com:anton-matosov/conan.git $TMP/conan
+git clone git@github.com:conan-io/conan-package-tools.git $TMP/conan-package-tools
+
+BASEDIR=$(dirname "$0")
+export PYTHONPATH="$TMP/conan:$TMP/conan-package-tools" $PYTHONPATH
+
+echo """
+#!/usr/bin/env python
+
+import sys
+sys.path.append('$TMP/conan')  # EDIT!!
+
+from conans.conan import main
+main(sys.argv[1:])
+""" > /usr/local/bin/conan
 
 conan user
 
-BASEDIR=$(dirname "$0")
-cp -f "$BASEDIR/settings.yml" "$HOME/.conan/"
