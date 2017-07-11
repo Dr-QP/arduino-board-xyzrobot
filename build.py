8c6a4dfc073d52c003b646f0a840b81e89a95a17
Anton Matosov <anton.matosov@gmail.com>
2017-07-10 22:33:40 -0700

    Configure proper auto-build. Fix example

--- build.py
+++ build.py
@@ -2,6 +2,16 @@ from conan.packager import ConanMultiPackager
 
 
 if __name__ == "__main__":
-    builder = ConanMultiPackager(username="anton-matosov", channel="stable")
-    builder.add_common_builds()
+    builder = ConanMultiPackager(args="--build missing", username="anton-matosov", channel="stable")
+    # builder.add_common_builds()
+    builder.add(settings={
+        "os": "Arduino",
+        "os.board": "XYZrobot1280",
+        "compiler": "gcc",
+        "compiler.version": "4.9",
+        "compiler.libcxx": "libstdc++11",
+        "arch": "avr"
+    }, env_vars={
+        "CC": "gcc"
+    })
     builder.run()
