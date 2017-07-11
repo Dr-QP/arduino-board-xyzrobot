35acf9fe6af557ebd5fd8cf74084c3696b66b8ea
Anton Matosov <anton.matosov@gmail.com>
2017-07-11 00:26:03 -0700

    Fix install to use proper path and install arduino on linux

--- .travis/install.sh
+++ .travis/install.sh
@@ -3,39 +3,44 @@
 set -e
 set -x
 
-if [[ "$(uname -s)" == 'Darwin' ]]; then
-    brew update || brew update
-    brew outdated pyenv || brew upgrade pyenv
-    brew install pyenv-virtualenv
-    brew install cmake || true
+if [[ $TRAVIS == true ]]; then
+    if [[ "$(uname -s)" == 'Darwin' ]]; then
+        brew update || brew update
+        brew outdated pyenv || brew upgrade pyenv
+        brew install pyenv-virtualenv
+        brew install cmake || true
 
-    # brew tap caskroom/versions
-    brew cask install arduino
+        # brew tap caskroom/versions
+        brew cask install java
+        brew cask install arduino
 
-    if which pyenv > /dev/null; then
-        eval "$(pyenv init -)"
+        if which pyenv > /dev/null; then
+            eval "$(pyenv init -)"
+        fi
+
+        pyenv install 2.7.10
+        pyenv virtualenv 2.7.10 conan
+        pyenv rehash
+        pyenv activate conan
+    else
+        sudo apt-get update 
+        sudo apt-get install arduino arduino-core
     fi
-
-    pyenv install 2.7.10
-    pyenv virtualenv 2.7.10 conan
-    pyenv rehash
-    pyenv activate conan
 fi
 
 # pip install conan --upgrade
 # pip install conan_package_tools
 
+TMP=$(mktemp -d)
 git clone git@github.com:anton-matosov/conan.git $TMP/conan
 git clone git@github.com:conan-io/conan-package-tools.git $TMP/conan-package-tools
 
-BASEDIR=$(dirname "$0")
-export PYTHONPATH="$TMP/conan:$TMP/conan-package-tools" $PYTHONPATH
+export PYTHONPATH="$TMP/conan:$TMP/conan-package-tools:$PYTHONPATH"
 
-echo """
-#!/usr/bin/env python
+echo -n """#!/usr/bin/env python
 
 import sys
-sys.path.append('$TMP/conan')  # EDIT!!
+sys.path.append('$TMP/conan')
 
 from conans.conan import main
 main(sys.argv[1:])
